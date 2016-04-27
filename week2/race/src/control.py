1df25aaf3c0dfce7ad6b86f04f9885ceb419f613
samm81 <samwmaynard@gmail.com>
2016-04-27 16:54:43 -0500

    week2 work

--- week2/race/src/control.py
+++ week2/race/src/control.py
@@ -3,12 +3,15 @@
 import rospy
 from race.msg import drive_param
 from race.msg import pid_input
+import math
 
 kp = 14.0
 kd = 0.09
 servo_offset = 18.5
 prev_error = 0.0 
-vel_input = 25.0
+vel_input = 10.0
+pwm_tick = 0
+pwm_freq = 4
 
 pub = rospy.Publisher('drive_parameters', drive_param, queue_size=1)
 
@@ -17,8 +20,10 @@ def control(data):
 	global vel_input
 	global kp
 	global kd
+	global pwm_tick
+	global pwm_freq
 
-	pid_error = data[0]
+	pid_error = data.pid_error
 	## Your code goes here
 	# 1. Scale the error
 	# 2. Apply the PID equation on error
@@ -28,15 +33,24 @@ def control(data):
 
 	angle = error + errordot
 
-	if angle > (math.pi / 4):
-		angle = math.pi / 4
-	elif angle < -(math.pi / 4):
-		angle = -(math.pi / 4)
+	if angle > 100:
+		angle = 100
+	elif angle < -100:
+		angle = -100
 
 	prev_error = pid_error
 
+	print "pid_error {}\nangle {}".format(pid_error, angle)
+	
+	vel = vel_input
+	if pwm_tick is pwm_freq:
+		pwm_tick = 0
+		#vel = 0
+	else:
+		pwm_tick += 1
+
 	msg = drive_param();
-	msg.velocity = vel_input	
+	msg.velocity = vel
 	msg.angle = angle
 	pub.publish(msg)
 
--- week2/race/src/dist_finder.py
+++ week2/race/src/dist_finder.py
@@ -6,7 +6,7 @@ from sensor_msgs.msg import LaserScan
 from race.msg import pid_input
 
 desired_trajectory = 1
-vel = 30
+vel = 5
 
 pub = rospy.Publisher('error', pid_input, queue_size=10)
 
@@ -16,32 +16,44 @@ pub = rospy.Publisher('error', pid_input, queue_size=10)
 def getRange(data,theta):
     """ Find the index of the arary that corresponds to angle theta.
     Return the lidar scan value at that index
-    Do some error checking for NaN and ubsurd values """
+    Do some error checking for NaN and absurd values """
+    car_theta = math.radians(theta) - math.pi / 2
+    if car_theta > 3 * math.pi / 4:
+        car_theta = 3 * math.pi / 4
+    elif car_theta < -3 * math.pi / 4:
+        car_theta = -3 * math.pi / 4
 
-    thetap = math.radians(theta) + (3 * math.pi / 4)
-    index = thetap / 0.004363
-    d = data[index]
-    return 
+    float_index = (car_theta + 3 * math.pi / 4) / data.angle_increment
+    index = int(float_index)
+    return data.ranges[index]
+    
 
 def callback(data):
-	theta = 50;
-	a = getRange(data,theta)
-	b = getRange(data,0)
-	swing = math.radians(theta)
-	
-        alpha = math.atan2( a * math.cos(theta) - b , a * math.sin(theta) )
-        AB = b * math.cos(alpha)
+    theta = 50;
+    a = getRange(data, theta)
+    b = getRange(data, 0)
+    swing = math.radians(theta)
 
-        error = desired_trajectory - AB
+    alpha = math.atan2( a * math.cos(swing) - b , a * math.sin(swing) )
+    AB = b * math.cos(alpha)
 
-	msg = pid_input()
-	msg.pid_error = error
-	msg.pid_vel = vel
-	pub.publish(msg)
-	
+    error = AB - desired_trajectory
+
+    #ABprime = a * math.cos(math.asin(a * math.sin(swing) / math.sqrt(a*a + b*b - 2*a*b*math.cos(swing))) - math.pi / 2 + swing)
+
+    print "a {}\nb {}".format(a, b)
+    print "AB {}".format(AB)
+    #print "ABprime {}".format(ABprime)
+    print "error {}".format(error)
+
+    msg = pid_input()
+    msg.pid_error = error
+    msg.pid_vel = vel
+    pub.publish(msg)
+    
 
 if __name__ == '__main__':
-	print("Laser node started")
-	rospy.init_node('dist_finder',anonymous = True)
-	rospy.Subscriber("scan",LaserScan,callback)
-	rospy.spin()
+    print("Laser node started")
+    rospy.init_node('dist_finder',anonymous = True)
+    rospy.Subscriber("scan",LaserScan,callback)
+    rospy.spin()
